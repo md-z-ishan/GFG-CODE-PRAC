79d5869b288f7e3ad48c0a7500dadff74b307052
Md Zishan <150007240+md-z-ishan@users.noreply.github.com>
2026-06-24 15:25:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -40,3 +40,55 @@ class Solution {
 		
 	}
 };
+//brute force method 
+// class Solution{
+//     public:
+//     int inversionCount(vector<int> &arr){
+//         int n=arr.size();
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[i]>arr[j]){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+    
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

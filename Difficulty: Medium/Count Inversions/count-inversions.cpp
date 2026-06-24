cbc35b4e9e8469beab8cfca4e65c9ae4a1dfa538
Md Zishan <150007240+md-z-ishan@users.noreply.github.com>
2026-06-24 14:58:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -0,0 +1,42 @@
+class Solution {
+	public:
+	long long count = 0;
+	void merge(vector<int> &arr, int left, int mid, int right) {
+		int i = left;
+		int j = mid + 1;
+		vector<int>ans;
+		while (i<=mid && j<=right) {
+			if (arr[i] <= arr[j]) {
+				ans.push_back(arr[i++]);
+			} else {
+				// yahi count karenge
+				count += (mid - i + 1);
+				ans.push_back(arr[j++]);
+			}
+		}
+		while (i<=mid) {
+			ans.push_back(arr[i++]);
+		}
+		while (j<=right) {
+			ans.push_back(arr[j++]);
+		}
+		for(int k=0;k<ans.size();k++){
+		    arr[left+k]=ans[k];
+		}
+	}
+	void divide(vector<int> &arr, int left, int right) {
+		int mid = left + (right - left)/2;
+		if (left<right) {
+			divide(arr, left, mid);
+			divide(arr, mid + 1, right);
+			merge(arr, left, mid, right);
+		}
+	}
+	int inversionCount(vector<int> &arr) {
+		// Code Here
+		count=0;
+		divide(arr, 0, arr.size() - 1);
+		return count;
+		
+	}
+};

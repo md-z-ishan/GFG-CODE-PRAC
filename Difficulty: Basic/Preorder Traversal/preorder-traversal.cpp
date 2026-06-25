bedf8bc6446d3859f390c56b7eaea759fce362cb
Md Zishan <150007240+md-z-ishan@users.noreply.github.com>
2026-06-25 16:35:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.cpp
@@ -0,0 +1,39 @@
+/*
+class Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    vector<int> preOrder(Node* root) {
+
+        vector<int> ans;
+
+        solve(root, ans);
+
+        return ans;
+    }
+
+    void solve(Node* root, vector<int> &ans) {
+
+        if (root == NULL) {
+            return;
+        }
+
+        ans.push_back(root->data);
+
+        solve(root->left, ans);
+
+        solve(root->right, ans);
+    }
+};

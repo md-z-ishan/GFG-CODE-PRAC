5ae98c0ffe04e35148285daf6bf1d8af91df302e
Md Zishan <150007240+md-z-ishan@users.noreply.github.com>
2026-08-24 12:35:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of odd and even elements/sum-of-odd-and-even-elements.cpp
+++ Difficulty: Basic/Sum of odd and even elements/sum-of-odd-and-even-elements.cpp
@@ -3,18 +3,16 @@ class Solution {
   public:
     vector<int> findSum(int n) {
         // Code here
-        int oddSum=0;
-        int evenSum=0;
-        for(int i=1;i<=n;i++){
-            if(i%2==0){
-                evenSum=evenSum+i;
-        
-            }
-            else{
-                oddSum=oddSum+i;
-            }
-            
-        }
-        return{oddSum,evenSum};
-    }
+     int o=0;
+     int e=0;
+     for(int i=0;i<=n;i++){
+         if(i%2==0){
+             e=e+i;
+         }
+         else{
+             o=o+i;
+         }
+     }
+     return{o,e};
+}
 };
